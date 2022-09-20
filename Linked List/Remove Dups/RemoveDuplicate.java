71db79fe6b629bca2a6281b41cd6d78c843bed56
Akshay Kumar <54402124+akshay161099@users.noreply.github.com>
2022-09-20 10:10:23 +0530

    Create RemoveDuplicate.java

---
+++ Linked List/Remove Dups/RemoveDuplicate.java
@@ -0,0 +1,53 @@
+
+
+public class RemoveDuplicate {
+    static class Node{
+        int data;
+        Node next;
+        Node(int d)
+        {
+            this.data = d;
+            next = null;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,2,3,1,6,7,5,2};
+        Node head = new Node(0);
+        Node newhead = head;
+        for(int i = 0;i<arr.length;i++) {
+            newhead.next = new Node(arr[i]);
+            newhead = newhead.next;
+        }
+        head = head.next;
+
+        Node head1 = remove(head);
+        Node curr = head1;
+        while(curr.next!=null){
+            System.out.print(curr.data+" -> ");
+            curr = curr.next;
+        }
+        System.out.print(curr.data);
+
+    }
+    public static Node remove(Node head){
+        Node curr = head;
+        while(curr!=null){
+            Node temp = curr;
+
+            while(temp.next!=null){
+                if(temp.next.data == curr.data){
+                    temp.next = temp.next.next;
+                }
+                else{
+                    temp = temp.next;
+                }
+
+
+
+            }
+            curr = curr.next;
+        }
+        return head;
+    }
+}
+
